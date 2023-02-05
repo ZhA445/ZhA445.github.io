ffce11fec0e2be362a596fcfc5ed2c0e2a5c7a4d
Zha445 <zinhtetaung2017z@gmail.com>
2023-02-05 22:50:11 +0630

    heart

---
+++ JQ/Vue/myapp/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <h2>Zin Htet Aung</h2>
+  <Testing :age="age">{{age}}</Testing>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+import Testing from './components/Testing.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      age: 21,
+    }
+  },
+  components:{
+      Testing
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ JQ/Vue/myapp/src/components/Testing.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2 class="text-danger">This is Testing VUE, Child Component!</h2>
+</template>
+
+<script>
+    export default {
+        name: 'TestingPart',
+        props:{
+            age: Number,
+        },
+        data(){
+            return{
+                name:'Zin Htet',
+                feet:5,
+                inches: 6,
+            }
+        }
+}
+</script>
+
+<style>
+
+</style>
